d46ec1eda0073fbee4008aa17215c08e97555594
armen-asriyan <armenasriyan@proton.me>
2024-11-06 13:19:28 +0100

    added Strings exercises

---
+++ 001.Strings/script.js
@@ -0,0 +1,55 @@
+// Exercice 0
+
+console.log("JavaScript est super");
+
+// Exercice 1
+
+console.log("JavaScript c'est facile");
+
+// Exercice 2
+
+console.log(
+  "C'est Lao Tseu qui a dit : \"L'échec est le fondement de la réussite\""
+);
+
+// Exercice 3
+
+console.log("Afficher un \\ n'est pas si évident");
+
+// Exercice 4
+
+console.log(
+  `Ceci est la première ligne
+Ceci est la deuxième ligne`
+);
+
+// Exercice 5
+
+console.log(
+  `Un jour le JavaScript n'aura plus de secrets pour moi
+Lire du code ne sera plus une gageure mais une joie
+Mes doigts seront habitués à tous ces symboles particuliers
+Et ma créativité de développeur pourra enfin s'exprimer`
+);
+
+// Exercice 6
+
+let name = `Bob`;
+
+console.log(
+  `Bonjour`,
+  name +
+    `, aujourd'hui nous faisons une promo \"la fidélité paye" !
+Tom doit écrire une chaîne de caractère qui permet d'écrire cette phrase pour chaque client.
+
+Pour l\'instant il essaye de faire fonctionner son code avec le prénom "Bob"`
+);
+
+// Exercice 7
+
+console.log(
+  `\tBonjour`,
+  name,
+  `Aujourd'hui nous faisons une promo,
+la fidélité paye !`
+);
