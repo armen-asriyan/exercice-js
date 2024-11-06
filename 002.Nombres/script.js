0754e891e6c17fe935049eb83b297991ccb172af
armen-asriyan <armenasriyan@proton.me>
2024-11-06 15:32:43 +0100

    Nombres added

---
+++ 002.Nombres/script.js
@@ -0,0 +1,46 @@
+// Exercice 0
+
+console.log("Exercice 0 : ", 28 + 6);
+
+// Exercice 1
+
+console.log("Exercice 1 : ", (6 * 4 + 2) / 2);
+
+// Exercice 2
+
+const magiciensDuCode = 1337;
+const lettresHugo = 256;
+const letterPrice = 2;
+
+console.log("Exercice 2 : ", (magiciensDuCode + lettresHugo) * letterPrice);
+
+// Exercice 3
+
+const budget = 3000;
+const facture = 1.5;
+const supplement = 500;
+
+console.log(
+  "Exercice 3 : ",
+  (magiciensDuCode + lettresHugo) * 1.5 + supplement
+);
+
+// Exercice 4
+
+const temperatureActuelle = 23;
+const temperatureMaximale = 35;
+const augmentation = 2;
+const jours = 3;
+
+console.log(
+  "Exercice 4 : ",
+  ((temperatureMaximale - temperatureActuelle) / augmentation) * jours
+);
+
+// Exercice 5
+
+const budgetHugo = 2350;
+const argentAmis = 1000;
+const taxesPercentage = (2350 * 12) / 100;
+
+console.log("Exercice 5 : ", budgetHugo + argentAmis - taxesPercentage);
